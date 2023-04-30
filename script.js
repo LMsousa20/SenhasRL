d8889e88a8028e99a3f8e9c664c41ace7b3c7ac0
lucas <lmsousa20@gmail.com>
2023-04-29 22:36:59 -0300

    incluasao-de-zap

--- script.js
+++ script.js
@@ -3,53 +3,71 @@ let StringDia = dia.getDate();
 let StringMes = dia.getMonth() + 1;
 let ano = dia.getYear();
 let hour = dia.getHours();
-let vl1 = (StringDia * 100) + StringMes;
-
-if(Number(localStorage.getItem("cdg1")) == 0){
-    let codigo = Number(prompt('Qual seu Codigo?')); 
-    localStorage.setItem("cdg1", codigo); 
+let vl1 = StringDia * 100 + StringMes;
+// Salvar o valor na memoria
+if (Number(localStorage.getItem("cdg1")) == 0) {
+  let codigo = Number(prompt("Qual seu Codigo?"));
+  localStorage.setItem("cdg1", codigo);
 }
-
+// Senha do sistem
 let codigoUser = localStorage.getItem("cdg1");
-let preValor = (vl1 / codigoUser) *10000;
-
-
-if (StringMes <= 9){
-    document.getElementById('data').innerHTML = `${StringDia}/0${StringMes}`;
-}else{
-    document.getElementById('data').innerHTML = `${StringDia}/${StringMes}`;
+let preValor = (vl1 / codigoUser) * 10000;
+// Imprimir a data
+if (StringMes <= 9) {
+  document.getElementById("data").innerHTML = `${StringDia}/0${StringMes}`;
+} else {
+  document.getElementById("data").innerHTML = `${StringDia}/${StringMes}`;
 }
-document.getElementById('result').innerHTML = parseInt(preValor);
+document.getElementById("result").innerHTML = parseInt(preValor);
+// Calculo da companytec
+function CalcCompanytec() {
+  let companytec = ano - StringMes - StringDia - hour + 1900;
+  document.getElementById("cmptc").innerHTML = companytec;
+}
+// Cosseno - Calculo da Eztech
+function cosseno() {
+  let cdgEztech = document.getElementById("inEztech").value;
+  if (cdgEztech == 0) {
+    return;
+  }
+  let senhaEztech = parseInt(Math.cos(Number(cdgEztech)) * 1000);
+  console.log(senhaEztech);
+  if (senhaEztech < 0) {
+    senhaEztech = senhaEztech * -1;
+  }
+  document.getElementById(
+    "resulteztc"
+  ).innerHTML = `<p>Senha do Eztech</p><div class="subdiv move">${senhaEztech}</div>`;
+}
+// Stilo
+function modDark() {
+  document.getElementById("conteiner1").style.background = "black";
+  document.getElementById("conteiner1").style.color = "white";
+}
 
-
-function CalcCompanytec(){
-let companytec = ano - StringMes - StringDia - hour + 1900;
-document.getElementById('cmptc').innerHTML = companytec;
+function modClean() {
+  document.getElementById("conteiner1").style.background = "white";
+  document.getElementById("conteiner1").style.color = "black";
 }
 
 
-function cosseno(){
-    let cdgEztech = document.getElementById('inEztech').value;
-    if(cdgEztech == 0){
-        return;
-    }
-    let senhaEztech = parseInt(Math.cos(cdgEztech)*1000);
-    if(senhaEztech <0){
-        senhaEztech = senhaEztech * -1;
-    }
+function zap() {
+  let cdgZap = document.getElementById("inZap").value;
+  console.log(typeof cdgZap);
+  console.log(cdgZap);
+  var numzap = cdgZap.replaceAll("-", "");
+  var numzap1 = numzap.replaceAll(" ", "");
+  var numzap2 = numzap1.replaceAll("(", "");
+  var numzap3 = numzap2.replaceAll(")", "");
+  var numFinal = numzap3.replaceAll("+", "");
+  console.log(typeof numFinal);
+  console.log(numFinal);
   
-    document.getElementById('resulteztc').innerHTML = `<p>Senha do Eztech</p><div class="subdiv move">${senhaEztech}</div>`;
-    
-};
+  document.getElementById("inZap").value = numFinal;
 
-function modDark(){
-    document.getElementById('conteiner1').style.background = 'black';
-    document.getElementById('conteiner1').style.color = 'white';
-
-}
-
-function modClean(){
-    document.getElementById('conteiner1').style.background = 'white';
-    document.getElementById('conteiner1').style.color = 'black';
-
+  document.getElementById("btnZap").innerHTML = `
+    <a href="https://api.whatsapp.com/send/?phone=%2B${numFinal}">
+    <input class="subdiv move" target="_blank" type="button" value="WhattsApp acima"></input>
+    </a>
+    `;
 }
